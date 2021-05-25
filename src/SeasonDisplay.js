63e21a87bcdfce7704a2760ec436697ba2f5e9a0
Dan Chiev <danchiev2@Gmail.com>
2021-05-24 20:00:43 -0400

    refractored seasonal display logic

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,12 @@
 import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun",
+  },
+  winter: { text: "Its cold", iconName: "snowflake" },
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -8,12 +16,12 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const icon = season === "winter " ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      <i className={`${icon} icon`} />
-      <h1>{season === "winter" ? "Its winter" : "its hot"}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
