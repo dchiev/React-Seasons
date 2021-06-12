af834351e5040e3d3d5f0f06d84f7be2cb545000
Dan Chiev <danchiev2@Gmail.com>
2021-06-12 15:01:12 -0400

    restructured

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ class App extends React.Component {
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -20,6 +19,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
